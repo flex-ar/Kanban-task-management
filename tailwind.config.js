d7ab662095a7f68173ee5f4f1c69cd3be6683f10
flex-ar <jmarnedo8@gmail.com>
2023-11-07 16:59:14 -0300

    add transition property

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        hide: 'transform, opacity, background-color, color, fill',
+        padding: 'padding',
+      },
+    },
   },
   plugins: [],
-}
-
+};
