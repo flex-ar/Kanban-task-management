bbc595ac5e10be3862a482bdf4bfc0fb96f59f8b
flex-ar <jmarnedo8@gmail.com>
2023-11-07 17:31:08 -0300

    refactor Sidebar commponent

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,66 +1,62 @@
-import { IconAddTask } from '../assets/IconAddTaskMobile';
-import { IconBoard } from '../assets/IconBoard';
-import { IconDarkTheme } from '../assets/IconDarkTheme';
-import { IconHideSidebar } from '../assets/IconHideSidebar';
-import { IconLightTheme } from '../assets/IconLightTheme';
+import { IconAddTask } from './icons/IconAddTaskMobile';
+import { IconDarkTheme } from './icons/IconDarkTheme';
+import { IconLightTheme } from './icons/IconLightTheme';
+import { useThemeContext } from '../ThemeContext';
+import BoardButton from './buttons/BoardButton';
 
-function Sidebar() {
+interface Props {
+  hide: boolean;
+}
+function Sidebar({ hide }: Props) {
+  const { theme, toggleTheme } = useThemeContext();
+  const totalBoards = 3;
+
   return (
-    <div className="flex flex-col justify-between pe-6 font-bold dark:bg-slate-800">
-      <div className="flex flex-col">
+    <nav
+      className={`fixed top-20 z-10 hidden h-[calc(100%-5rem)] w-72 pe-2 font-bold transition-transform dark:bg-slate-800 sm:flex sm:flex-col sm:justify-between ${
+        hide ? '-translate-x-full' : ''
+      }`}
+    >
+      <div className="flex flex-col overflow-auto">
         <p className="py-7 ps-6 text-xs tracking-widest dark:text-slate-400">
-          ALL BOARDS (3)
+          ALL BOARDS ({totalBoards})
         </p>
-        <button className="flex items-center gap-4 rounded-r-full fill-white py-3  ps-6 dark:bg-sky-500 dark:hover:bg-sky-400">
-          <span>
-            <IconBoard />
-          </span>
-          Platform Launch
-        </button>
-        <button className="flex items-center gap-4 rounded-r-full fill-slate-400 py-3  ps-6 dark:text-slate-400 dark:hover:bg-slate-700">
-          <span>
-            <IconBoard />
-          </span>
-          Marketing Plan
-        </button>
-        <button className="flex items-center gap-4 rounded-r-full fill-slate-400 py-3 ps-6 dark:text-slate-400 dark:hover:bg-slate-700">
-          <span>
-            <IconBoard />
-          </span>
-          Roadmap
-        </button>
-        <button className="flex items-center gap-4 rounded-r-full fill-sky-400 py-3 pe-10 ps-6 dark:text-sky-400 dark:hover:bg-slate-600">
-          <span>
-            <IconBoard />
-          </span>
+        <div className="flex flex-col overflow-auto pe-2">
+          <BoardButton active>Platform Launch</BoardButton>
+          <BoardButton>Marketing Plan</BoardButton>
+          <BoardButton>Roadmap</BoardButton>
+        </div>
+        <BoardButton
+          variant="ghost-v2"
+          className={`dark:fill-sky-400 dark:text-sky-400 ${
+            totalBoards > 13 ? 'me-4' : 'me-2'
+          }`}
+        >
           <div className="flex items-center gap-1">
             <span>
               <IconAddTask />
             </span>
             <span>Create New Board</span>
           </div>
-        </button>
+        </BoardButton>
       </div>
-      <div className="mb-20 ms-5 flex items-center justify-center gap-6 rounded-md px-5 py-3 dark:bg-slate-900">
+      <div
+        className={`mb-20 ms-5 mt-5 flex items-center justify-center gap-6 rounded-md px-5 py-3 dark:bg-slate-900 ${
+          totalBoards > 13 ? 'me-4' : 'me-2'
+        }`}
+      >
         <IconLightTheme />
-        <label className="flex w-10 cursor-pointer justify-end rounded-full bg-sky-400 p-1">
-          <input
-            type="checkbox"
-            onChange={() => console.log('THEME')}
-            className="hidden"
+        <label className="relative w-10 cursor-pointer rounded-full bg-sky-500 p-1">
+          <input type="checkbox" onChange={toggleTheme} className="hidden" />
+          <div
+            className={`h-3.5 w-3.5 rounded-full bg-white transition-transform ${
+              theme === 'dark' ? 'translate-x-[18px]' : ''
+            }`}
           />
-          <div className="h-3.5 w-3.5 rounded-full bg-white" />
         </label>
         <IconDarkTheme />
       </div>
-
-      <button className="absolute bottom-6 flex items-center gap-4 rounded-r-full fill-slate-400 py-3 pe-14 ps-6 hover:fill-sky-400 dark:text-slate-400 dark:hover:bg-white dark:hover:text-sky-400">
-        <span>
-          <IconHideSidebar />
-        </span>
-        Hide Sidebar
-      </button>
-    </div>
+    </nav>
   );
 }
 
